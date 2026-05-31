7290af375fea05c6b6571f0db806953dd53f6e2e
Gianluca De Bonis <gianluca.de.bonis@cern.ch>
2026-05-31 11:13:17 +0200

    Stop execution if validation fails

--- main.py
+++ main.py
@@ -8,38 +8,48 @@ from src.entity.config_entity import TrainingPipelineConfig
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
 
+
+def run_pipeline():
+    training_config = TrainingPipelineConfig()
+
+    # =========================================
+    # Data ingestion
+    # =========================================
+    di_config = DataIngestionConfig(training_config)
+    data_ingestion = DataIngestion(di_config)
+    logging.info('Initiate data ingestion.')
+    di_artifact = data_ingestion.initiate_data_ingestion()
+    logging.info('Data ingestion completed')
+    print(di_artifact)
+
+    # =========================================
+    # Data validation
+    # =========================================
+    dv_config = DataValidationConfig(training_config)
+    data_validation = DataValidation(di_artifact, dv_config)
+    logging.info('Initiate data validation')
+    dv_artifact = data_validation.initiate_data_validation()
+    logging.info('Data validation completed')
+    print(dv_artifact)
+
+    if not dv_artifact.validation_status:
+        logging.warning('Data validation failed. Skipping data transformation.')
+        return dv_artifact
+
+    # =========================================
+    # Data transformation
+    # =========================================
+    df_config = DataTransformationConfig(training_config)
+    data_transformation = DataTransformation(dv_artifact, df_config)
+    logging.info('Initiate data transformation')
+    df_artifact = data_transformation.initiate_data_transformation()
+    logging.info('Data transformation completed')
+    print(df_artifact)
+    return df_artifact
+
+
 if __name__ == '__main__':
     try:
-        training_config = TrainingPipelineConfig()
-
-        # =========================================
-        # Data ingestion
-        # =========================================
-        di_config = DataIngestionConfig(training_config)
-        data_ingestion = DataIngestion(di_config)
-        logging.info('Initiate data ingestion.')
-        di_artifact = data_ingestion.initiate_data_ingestion()
-        logging.info('Data ingestion completed')
-        print(di_artifact)
-
-        # =========================================
-        # Data validation
-        # =========================================
-        dv_config = DataValidationConfig(training_config)
-        data_validation = DataValidation(di_artifact, dv_config)
-        logging.info('Initiate data validation')
-        dv_artifact = data_validation.initiate_data_validation()
-        logging.info('Data validation completed')
-        print(dv_artifact)
-
-        # =========================================
-        # Data transformation
-        # =========================================
-        df_config = DataTransformationConfig(training_config)
-        data_transformation = DataTransformation(dv_artifact, df_config)
-        logging.info('Initiate data transformation')
-        df_artifact = data_transformation.initiate_data_transformation()
-        logging.info('Data transformation completed')
-        print(df_artifact)
+        run_pipeline()
     except Exception as e:
         raise NetworkSecurityException(e, sys)
